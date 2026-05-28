de91d3d418261ff8f4e9e1093a292ae215cfcf0e
vishnusreerag-tech <vishnusreeragvishnukv@gmail.com>
2026-05-28 17:26:48 +0530

    Switched Gemini to Groq API

--- utils/generator.py
+++ utils/generator.py
@@ -1,16 +1,12 @@
-import google.generativeai as genai
+from groq import Groq
 import os
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-genai.configure(
-    api_key=os.getenv("GOOGLE_API_KEY")
-)
-
-model = genai.GenerativeModel(
-    "gemini-2.5-flash"
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
 )
 
 
@@ -30,9 +26,17 @@ def generate_answer(query, documents):
 
     try:
 
-        response = model.generate_content(prompt)
+        response = client.chat.completions.create(
+            model="llama3-8b-8192",
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ]
+        )
 
-        return response.text
+        return response.choices[0].message.content
 
     except Exception as e:
 
